22f4eb4cfe6a4d969bd5d38a4f996c0e46563d70
Antti Löytynoja <aloytyno@mathworks.com>
2025-06-06 11:46:26 +0300

    Loading a presaved autoencoder for function generation

--- Tests/scriptTests.m
+++ Tests/scriptTests.m
@@ -1,10 +1,26 @@
 classdef scriptTests < matlab.unittest.TestCase
 
+    properties
+        prj
+    end
+
+    methods (TestMethodSetup)
+        function openProject(testCase)
+            try
+                testCase.prj = currentProject;
+                cd(testCase.prj.RootFolder)
+            catch
+                open("AutoEncoderAnomalyDetection.prj");
+                testCase.prj = currentProject;
+                cd(testCase.prj.RootFolder)
+            end
+        end
+    end
+
     methods (Test)
         % Test methods
 
         function smokeTest(testCase)
-            cd("..\")
             evalin("base","AnomalyDetectionDemo")
             % Verify that a script runs to the point where variable "ax2"
             % is created
