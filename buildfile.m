d4f9037ac5f71cdbd52412f8acaa2d57437ebffe
Antti Löytynoja <aloytyno@mathworks.com>
2025-06-09 11:40:50 +0300

    Changed script title

--- buildfile.m
+++ buildfile.m
@@ -4,7 +4,8 @@ import matlab.buildtool.tasks.*
 plan = buildplan(localfunctions);
 
 plan("check") = CodeIssuesTask;
-plan("test") = TestTask(Dependencies="check");
+plan("test") = TestTask(Dependencies="check",...
+    TestResults="test-results/results.xml");
 
 plan.DefaultTasks = ["check" "test"];
 end
